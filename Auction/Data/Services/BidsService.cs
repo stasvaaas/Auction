300424fdc6362134e18319c49d99dfcd8f7c2ad6
stasvaaas <105922844+stasvaaas@users.noreply.github.com>
2024-04-13 11:24:16 +0300

    Add project files.

---
+++ Auction/Data/Services/BidsService.cs
@@ -0,0 +1,27 @@
+﻿using Auction.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auction.Data.Services
+{
+    public class BidsService : IBidsService
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public BidsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(Bid bid)
+        {
+            _context.Bids.Add(bid);
+            await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<Bid> GetAll()
+        {
+            var applicationDbContext = from x in _context.Bids.Include(x => x.Listing).ThenInclude(x => x.User) select x;
+            return applicationDbContext;
+        }
+    }
+}
---
+++ Auction/Data/Services/CommentsService.cs
@@ -0,0 +1,21 @@
+﻿using Auction.Models;
+using System.Drawing.Text;
+
+namespace Auction.Data.Services
+{
+    public class CommentsService : ICommentsService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(Comment comment)
+        {
+            _context.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
